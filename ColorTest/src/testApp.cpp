1f31a7a4bfd8cdbe429ea57fb620cc6d408e0870
Matt Felsen <mattfelsen@gmail.com>
2014-01-21 17:17:25 -0500

    Updating color values from on-site projector test

--- ColorTest/src/testApp.cpp
+++ ColorTest/src/testApp.cpp
@@ -8,8 +8,8 @@ void testApp::setup(){
 	
 	mainOutputSyphonServer.setName("Color the Temple");
     
-    names.resize(25);
-    drawingsColors.resize(25);
+    names.resize(27);
+    drawingsColors.resize(27);
     
     names[0] = "Bg-white.svg";
     names[1] = "Bg-stripes.svg";
@@ -36,37 +36,54 @@ void testApp::setup(){
     names[22] = "white-glyphs.svg";
     names[23] = "light-blue-glyphs.svg";
     names[24] = "crowns.svg";
+	names[25] = "hathor-dress.svg";
+	names[26] = "horus-skin.svg";
    
 
+	ofColor white = ofColor(255,255,255);
+	ofColor black = ofColor(0, 0, 0);
+	ofColor blueDark = ofColor(36, 33, 68);
+	ofColor blueLight = ofColor(0, 140, 120);
+	ofColor greenKelly = ofColor(0, 157, 100);
+	ofColor greenGlyphs = ofColor(0,115,100);
+	ofColor red = ofColor(165,10,10);
+	ofColor redSkin = ofColor(140,70,32);
+	ofColor yellowGold = ofColor(180,160,30);
+	ofColor yellowBright = ofColor(255,200,0);
+	ofColor skin = ofColor(255,235,150);
+	ofColor brown = ofColor(30, 20, 20);
+	
     
-    drawingsColors[0] = ofColor(255,255,255);
-    drawingsColors[1] = ofColor(0, 170, 120);//ofColor(30,40,78);
-    drawingsColors[2] = ofColor(140,70,32);
-    drawingsColors[3] = ofColor(255,235,150);
-    drawingsColors[4] = ofColor(30,20,20);
-    drawingsColors[5] = ofColor(255,255,255);
-    drawingsColors[6] = ofColor(150, 60, 35); //150, 75, 60); // ofColor(3, 100, 32);
-    drawingsColors[7] = ofColor(190,160,40);
-    drawingsColors[8] = ofColor(0,115,100);
-    drawingsColors[9] = ofColor(0, 140, 120);
-    drawingsColors[10] = ofColor(0, 140, 120); //ofColor(0, 170, 120);//ofColor(30,40,80);
-    drawingsColors[11] = ofColor(255,200,0);
-    drawingsColors[12] = ofColor(0,80,80);// ofColor(0, 170, 120);
-    drawingsColors[13] = ofColor(255,255,255);
-    drawingsColors[14] = ofColor(165,10,10);
-    drawingsColors[15] = ofColor(255,200,0);
-    drawingsColors[16] = ofColor(0,0,0);
-    drawingsColors[17] = ofColor(0, 140, 120); //ofColor(0, 170, 120);//ofColor(30,40,80);
-    drawingsColors[18] = ofColor(180,160,30);//ofColor(255,200,0);
-    drawingsColors[19] = ofColor(165,10,10);
-    drawingsColors[20] = ofColor(0,115,100);
-    drawingsColors[21] = ofColor(30,20,17);
-    drawingsColors[22] = ofColor(255,255,255);
-    drawingsColors[23] = ofColor(255,200,0);
-    drawingsColors[24] = ofColor(10, 120, 32);
+    drawingsColors[0] = white;
+    drawingsColors[1] = blueDark; //ofColor(0, 170, 120);//ofColor(30,40,78);
+    drawingsColors[2] = redSkin;
+    drawingsColors[3] = skin;
+    drawingsColors[4] = brown;
+    drawingsColors[5] = white;
+    drawingsColors[6] = red; //150, 75, 60); // ofColor(3, 100, 32);
+    drawingsColors[7] = yellowGold; // ofColor(190,160,40);
+    drawingsColors[8] = greenKelly; // ofColor(0,115,100);
+    drawingsColors[9] = blueDark;//ofColor(0, 140, 120);
+    drawingsColors[10] = blueDark;//ofColor(0, 140, 120); //ofColor(0, 170, 120);//ofColor(30,40,80);
+    drawingsColors[11] = yellowGold; // ofColor(255,200,0);
+    drawingsColors[12] = blueDark;//ofColor(0,80,80);// ofColor(0, 170, 120);
+    drawingsColors[13] = white;
+    drawingsColors[14] = red;
+    drawingsColors[15] = yellowGold;// ofColor(255,200,0);
+    drawingsColors[16] = black;
+    drawingsColors[17] = blueLight; //ofColor(0, 170, 120);//ofColor(30,40,80);
+    drawingsColors[18] = yellowGold;
+    drawingsColors[19] = red;
+    drawingsColors[20] = greenGlyphs;
+    drawingsColors[21] = brown; //ofColor(30,20,17);
+    drawingsColors[22] = white;
+    drawingsColors[23] = yellowGold; // ofColor(255,200,0);
+    drawingsColors[24] = greenKelly; //ofColor(10, 120, 32);
+	drawingsColors[25] = blueDark;
+	drawingsColors[26] = blueLight;
 
 
-	for(int i = 0; i < 25; i++){
+	for(int i = 0; i < drawingsColors.size(); i++){
         
         
         MLSVGObject h;
